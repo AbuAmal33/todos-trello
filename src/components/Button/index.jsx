69cceb0601125ea0e4a174f102affac22bcf771b
Ibrahim <i0978@bk.ru>
2021-01-24 18:52:03 +0200

    'added-new-branch'

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './button.css';
+import PropTypes from 'prop-types';
+
+function Button(props) {
+  return (
+    <button
+      className={props.size}
+      onClick={props.onClick}
+      style={{ borderRadius: props.radius }}
+      disabled={props.todo.deleting}
+    >
+      {props.children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  radius: PropTypes.number,
+  onClick: PropTypes.func.isRequired,
+};
+
+Button.defaultProps = {
+  size: 'small',
+  radius: 50,
+};
+
+export default Button;
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -17,13 +17,41 @@ export const loadTodos = () => {
 
 export const removeTodo = (id) => {
   return (dispatch) => {
+    dispatch({
+      type: 'todos/remove/start',
+      payload: id,
+    });
+
     fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
       method: 'DELETE',
     })
       .then((response) => response.json())
-      .then((json) => {
+      .then(() => {
         dispatch({
-          type: 'delete',
+          type: 'todos/remove/success',
+          payload: id,
+        });
+      });
+  };
+};
+
+export const CheckTodo = (id, completed) => {
+  return (dispatch) => {
+    dispatch({
+      type: 'todos/check/start',
+      payload: id,
+    });
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({ completed: !completed }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then(() => {
+        dispatch({
+          type: 'todos/check/success',
           payload: id,
         });
       });
